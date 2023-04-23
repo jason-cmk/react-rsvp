407efadc3d9c297c73507769e73a64d8a3ac94c2
Jason Kang <jas.kang@gmail.com>
2023-04-24 00:19:21 +1200

    Update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,11 @@ import InvitationMessage from './InvitationMessage'
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-rose-400 to-rose-200">
-            <div className="rounded-md columns-1 container bg-stone-50 text-black font-serif m-10 p-5 drop-shadow-md">
-                <img src='/charlie_rough.png' className='p-5'/>
+            <div className="rounded-md columns-1 container bg-stone-100 text-black font-serif m-10 p-5 drop-shadow-md">
+                <div className='flex justify-center items-center'>
+                    {/* eslint-disable-next-line */}
+                    <img src='/charlie_rough.png' alt='Charlie the cat' className='max-w-sm p-5'/>
+                </div>
                 <InvitationMessage />
                 <RSVPForm />
             </div>
