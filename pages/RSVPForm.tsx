117523399d983b9f3cea7f61547d466887c261ad
Jason Kang <jas.kang@gmail.com>
2023-04-24 00:19:14 +1200

    Add message to form

--- pages/RSVPForm.tsx
+++ pages/RSVPForm.tsx
@@ -1,19 +1,24 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react'
 
 function RSVPForm() {
-    const [attending, setAttending] = useState<string | null>(null);
-    const [foodAllergies, setFoodAllergies] = useState<string | null>(null);
+    const [attending, setAttending] = useState<string | null>(null)
+    const [foodAllergies, setFoodAllergies] = useState<string | null>(null)
+    const [message, setMessage] = useState<string | null>(null)
 
     function handleAttendingChange(event: ChangeEvent<HTMLSelectElement>) {
-        setAttending(event.target.value);
+        setAttending(event.target.value)
     }
 
     function handleFoodAllergiesChange(event: ChangeEvent<HTMLInputElement>) {
-        setFoodAllergies(event.target.value);
+        setFoodAllergies(event.target.value)
+    }
+
+    function handleMessageChange(event: ChangeEvent<HTMLInputElement>): void {
+        setMessage(event.target.value)
     }
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
+        event.preventDefault()
         fetch('/api/rsvp', {
             method: 'POST',
             headers: {
@@ -26,45 +31,51 @@ function RSVPForm() {
         })
             .then(response => response.json())
             .then(data => {
-                console.log('RSVP submitted successfully:', data);
+                console.log('RSVP submitted successfully:', data)
             })
             .catch(error => {
-                console.error('Error submitting RSVP:', error);
-            });
+                console.error('Error submitting RSVP:', error)
+            })
     }
 
     return (
-        <div className="flex min-h-full justify-center 
-        //bg-blue-50"
-        >
+        <div className='min-h-full flex justify-center items-center font-sans'>
             <form onSubmit={handleSubmit}
-                className="space-y-6 bg-stone-50 rounded-md drop-shadow-md p-5 grid-cols-1">
+                className='space-y-6 bg-stone-50 rounded-xl drop-shadow-xl p-5 grid-cols-1 '>
                 <div>
                     <label>
-                        Are you able to attend?
-                        <select value={attending || ''} onChange={handleAttendingChange} className='ml-3 border'>
-                            <option value=""></option>
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
+                        <div class='my-1'>Are you able to attend?</div>
+                        <select value={attending || ''} onChange={handleAttendingChange} className='border my-1'>
+                            <option value=''></option>
+                            <option value='yes'>Yes</option>
+                            <option value='no'>No</option>
                         </select>
                     </label>
                 </div>
                 <div>
                     <label>
-                        Food allergies:
-                        <input type="text" value={foodAllergies || ''} onChange={handleFoodAllergiesChange}
-                            className='ml-3 border'
+                        <div>Food allergies:</div>
+                        <input type='text' value={foodAllergies || ''} onChange={handleFoodAllergiesChange}
+                            className='border my-1'
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <div>Leave us message!</div>
+                        <textarea type='text' value={message || ''} onChange={handleMessageChange}
+                            className='border my-1 ' 
                         />
                     </label>
                 </div>
                 <br />
-                <button type="submit"
-                    className='border bg-stone-50'>
+                <button type='submit'
+                    className='bg-rose-300 btn hover:bg-rose-100 font-bold py-2 px-4 rounded-full'>
                     Submit RSVP
                 </button>
             </form>
         </div>
-    );
+    )
 }
 
-export default RSVPForm;
+export default RSVPForm
