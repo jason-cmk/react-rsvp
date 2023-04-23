1c2eb8f934d3cea61db0a6c7919fe5cddd54807d
Jason Kang <jas.kang@gmail.com>
2023-04-23 14:05:08 +1200

    Update wording

--- pages/InvitationMessage.tsx
+++ pages/InvitationMessage.tsx
@@ -4,9 +4,11 @@ export default function InviationMessage() {
     return (
         <div className='grid-cols-1 py-5 items-center justify-center text-center'
         >
-        <div className=''>Dear Dale and Kate,
-            we are excited to invite you to our wedding on the <br />
-                <div className='text-4xl p-5'>14th of March 2023
+            <p className=''>Dear Dale and Kate</p>
+            <p className=''>We are excited to invite you to the wedding of</p>
+            <h1 className='text-4xl p-5 text-center '>Cindy and Jason</h1>
+            <div className=''>on the<br />
+                <div className='text-4xl p-5'>14th of March 2024
                 </div>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,6 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-rose-400 to-rose-200">
             <div className="rounded-md columns-1 container bg-stone-50 text-black font-serif m-10 p-5 drop-shadow-md">
                 <img src='/charlie_rough.png' className='p-5'/>
-                <p className='text-center'>You are invited to the wedding of</p>
-                <h1 className='text-4xl p-5 text-center '>Cindy and Jason</h1>
                 <InvitationMessage />
                 <RSVPForm />
             </div>
