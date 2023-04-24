32a671c17caaca2025b3f1b0becdb13854c15f30
Jason Kang <jas.kang@gmail.com>
2023-04-24 12:25:51 +1200

    Clean up form structure

--- pages/RSVPForm.tsx
+++ pages/RSVPForm.tsx
@@ -39,42 +39,34 @@ function RSVPForm() {
     }
 
     return (
-        <div className='min-h-full flex justify-center items-center font-sans'>
-            <form onSubmit={handleSubmit}>
-                <div className='space-y-6 bg-stone-50 rounded-xl drop-shadow-xl p-5 grid-cols-1 w-96'>
-                    <div>
-                        <label className='flex-col'>
-                            <div className='w-full'>Are you able to attend?</div>
-                            <select value={attending || ''} onChange={handleAttendingChange} className='w-full my-1 rounded-lg border border-gray-300'>
-                                <option value=''></option>
-                                <option value='yes'>Yes</option>
-                                <option value='no'>No</option>
-                            </select>
-                        </label>
-                    </div>
-                    <div>
-                        <label className='flex-col'>
-                            <div className='w-full'>Food allergies:</div>
-                            <input type='text' value={foodAllergies || ''} onChange={handleFoodAllergiesChange}
-                                className='my-1 w-full rounded-lg border border-gray-300'
-                            />
-                        </label>
-                    </div>
-                    <div>
-                        <label className='flex-col'>
-                            <div className='w-full'>Leave us message!</div>
-                            <textarea value={message || ''} rows={5} onChange={handleMessageChange}
-                                className='my-1 w-full rounded-lg border border-gray-300'
-                            />
-                        </label>
-                    </div>
-                    <button type='submit'
-                        className='bg-rose-300 btn hover:bg-rose-100 font-medium py-2 px-4 rounded-full'>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit} className='min-h-full flex justify-center items-center font-sans'>
+            <div className='space-y-6 bg-stone-50 rounded-xl drop-shadow-xl p-5 grid-cols-1 w-96'>
+                <label className='flex-col'>
+                    <div className='w-full'>Are you able to attend?</div>
+                    <select value={attending || ''} onChange={handleAttendingChange} className='w-full my-1 rounded-lg bg-white border border-gray-300'>
+                        <option value=''></option>
+                        <option value='yes'>Yes</option>
+                        <option value='no'>No</option>
+                    </select>
+                </label>
+                <label className='flex-col'>
+                    <div className='w-full'>Food allergies:</div>
+                    <input type='text' value={foodAllergies || ''} onChange={handleFoodAllergiesChange}
+                        className='my-1 w-full rounded-lg border border-gray-300'
+                    />
+                </label>
+                <label className='flex-col'>
+                    <div className='w-full'>Leave us message!</div>
+                    <textarea value={message || ''} rows={5} onChange={handleMessageChange}
+                        className='my-1 w-full rounded-lg border border-gray-300'
+                    />
+                </label>
+                <button type='submit'
+                    className='bg-rose-300 btn hover:bg-rose-100 font-medium py-2 px-4 rounded-full'>
+                    Submit
+                </button>
+            </div>
+        </form>
     )
 }
 
