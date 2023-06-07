907d5781c534ba00b8c69ea7f225f3f90a21cc56
Jason Kang <jas.kang@gmail.com>
2023-06-07 12:12:18 +1200

    Add loading blocks

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 
 async function getInvitationData(id: string) {
     const maxRetries = 5;
-    const delay = 5000;
+    const delay = 2000;
     let tries = 0;
 
     while (tries < maxRetries) {
@@ -78,6 +78,49 @@ export default function Home() {
                 }
                 {invitationData &&
                     <RSVPForm invitationData={invitationData} />
+                }
+
+                {!invitationData &&
+                    <div className="border border-slate-200 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                        <div className="animate-pulse flex space-x-4">
+                            <div className="flex-1 space-y-6 py-1">
+                                <div className="h-2 bg-slate-500 rounded"></div>
+                                <div className="space-y-3">
+                                    <div className="grid grid-cols-3 gap-4">
+                                        <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                        <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                    </div>
+                                    <div className="grid grid-cols-3 gap-4">
+                                        <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                        <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                    </div>
+                                    <div className="grid grid-cols-3 gap-4">
+                                        <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                        <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                    </div>
+
+
+                                    <div className="border border-slate-200 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                                        <div className="grid grid-cols-3 gap-4 my-3">
+                                            <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                            <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                        </div>
+                                        <div className="grid grid-cols-3 gap-4 my-3">
+                                            <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                            <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                        </div>
+                                        <div className="grid grid-cols-3 gap-4 my-3">
+                                            <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                                            <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                        </div>
+                                        <div className="grid grid-cols-5 gap-4 my-3">
+                                            <div className="h-2 bg-slate-500 rounded col-span-1"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 }
             </div>
         </main>
