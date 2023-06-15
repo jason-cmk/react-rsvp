3c62838d62fb8f9a7c52e4d73689ffba0720e15f
Jason Kang <jas.kang@gmail.com>
2023-06-15 15:59:57 +1200

    Set the damn title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="shortcut icon" href="/favicon.ico" />
+                <title>Cindy and Jason's Wedding!!!</title>
             </Head>
             <body>
                 <Main />
