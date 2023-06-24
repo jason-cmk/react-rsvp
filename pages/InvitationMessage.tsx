6239df6684a5402babd17526df51b496d35f4dbf
Jason Kang <jas.kang@gmail.com>
2023-06-24 14:54:59 +1200

    Add the location

--- pages/InvitationMessage.tsx
+++ pages/InvitationMessage.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 export default function InviationMessage(props: { invitee: string }) {
     return (
         <div className='grid-cols-1 py-5 items-center justify-center text-center' >
-            <p className=''>Dear <span className='italic'>{props.invitee}</span></p>
-            <p className='text-lg'>we are excited to invite you to the wedding of</p>
-            <h1 className='text-4xl p-4 text-center '>Cindy and Jason</h1>
-            <p className='text-lg'>on</p>
+            <p className='text-xl'>Dear <span className='italic'>{props.invitee}</span></p>
+            <p className='text-xl'>we are excited to invite you to celebrate the wedding of</p>
+            <h1 className='text-4xl p-4 text-center'>Cindy and Jason</h1>
+            <p className='text-xl'>on</p>
             <div className='text-4xl p-4 text-center'>Thursday 14th of March 2024</div>
+            <p className='text-xl'>at</p>
+            <div className='text-4xl p-4 text-center'>
+                <a href="https://goo.gl/maps/ftAd66DC2jx5NdHr6">Bridgewater Estate 🗺️📍</a>
+            </div>
         </div>
     )
 }
